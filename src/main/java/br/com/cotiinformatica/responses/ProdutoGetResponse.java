b27d12afb942a957effa9392ed232d5fd0ca5a3f
smendescoti <sergio.coti@gmail.com>
2022-02-15 21:43:42 -0300

    first commit

---
+++ src/main/java/br/com/cotiinformatica/responses/ProdutoGetResponse.java
@@ -0,0 +1,16 @@
+package br.com.cotiinformatica.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ProdutoGetResponse {
+
+	private Integer idProduto;
+	private String nome;
+	private String descricao;
+	private Double preco;
+	private Integer quantidade;
+	private Double total;	
+}
